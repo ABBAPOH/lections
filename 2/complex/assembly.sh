6be0ed28ff6bb6fcab84fadcf94429c13bfae683
Ivan Komissarov <ABBAPOH@gmail.com>
2016-09-18 20:16:19 +0300

    Refactor

--- 2/complex/assembly.sh
+++ 2/complex/assembly.sh
@@ -1,3 +1,3 @@
 #~/bin/bash
 
-clang++ -std=c++11 -S main.cpp -o main.asm
+clang++ -std=c++11 -S src/main.cpp -o main.asm
--- 2/complex/build.2.sh
+++ 2/complex/build.2.sh
@@ -1,5 +1,5 @@
 #~/bin/bash
 
-clang++ -std=c++11 -c main.cpp 
-clang++ -std=c++11 -c class.cpp
+clang++ -std=c++11 -c src/main.cpp
+clang++ -std=c++11 -c src/class.cpp
 clang++ -std=c++11 main.o class.o -o complex
