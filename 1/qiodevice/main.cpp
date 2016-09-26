69ac1b57b8ad001191bd81d1e01d2d415065c1f6
Ivan Komissarov <ABBAPOH@gmail.com>
2016-09-26 21:41:54 +0300

    Update QIODevice example

--- 1/qiodevice/main.cpp
+++ 1/qiodevice/main.cpp
@@ -1,29 +1,39 @@
-#include <QCoreApplication>
-#include <QFile>
-#include <QDebug>
-#include <QDataStream>
-#include <qbuffer.h>
+#include <QtCore/QCoreApplication>
+#include <QtCore/QFile>
+#include <QtCore/QDebug>
+#include <QtCore/QBuffer>
 
-void do_work(QIODevice *device)
+int do_work(QIODevice *device)
 {
     if (!device->open(QIODevice::ReadOnly)) {
         qWarning() << "Can't open device";
-        return;
+        return 1;
     }
 
     QByteArray data = device->readAll();
     qDebug() << data;
+    return 0;
 }
 
 int main(int argc, char *argv[])
 {
-    QFile file("/Users/arch/admin.txt");
-    if (file.size() < 1024*1024) {
+    if (argc != 2) {
+        qDebug() << "Usage: qiodevice filename";
+        return 0;
+    }
+
+    QFile file(QString::fromLocal8Bit(argv[1]));
+    // If file is small, read it to the buffer
+    if (file.size() < 10*1024*1024) {
+        if (!file.open(QIODevice::ReadOnly)) {
+            qWarning() << "Can't open file";
+            return 1;
+        }
         QByteArray data = file.readAll();
         QBuffer buf(&data);
-        do_work(&buf);
+        return do_work(&buf);
     } else {
-        do_work(&file);
+        return do_work(&file);
     }
     return 0;
 }
