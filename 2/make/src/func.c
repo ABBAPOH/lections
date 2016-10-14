4f776e9f1977b2f1ee8a5f0c19fc37f7bf8f5975
Ivan Komissarov <ABBAPOH@gmail.com>
2016-10-14 18:32:39 +0300

    Modify make example

---
+++ 2/make/src/func.c
@@ -0,0 +1,8 @@
+#include "func.h"
+
+#include <stdio.h>
+
+void function()
+{
+    printf("Hello from function\n");
+}
---
+++ 2/make/src/func.h
@@ -0,0 +1,6 @@
+#ifndef FUNC_H
+#define FUNC_H
+
+void function();
+
+#endif // FUNC_H
