6e073f8fab93612daf793014d9de62e1a10ae9fd
Ivan Komissarov <ABBAPOH@gmail.com>
2016-10-21 22:37:05 +0300

    Fix pointers example

--- 3/1.pointers/main.c
+++ 3/1.pointers/main.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
+#include <string.h>
 
-int main()
+void reverse(char *s, int len)
 {
-   int array[] = {0, 1, 2, 4, 5, 6, 7, 8, 9};
-   int value0 = array[0];
-   int *ptr0 = 0;
-   int *ptr = array;
-   int value1 = ptr[1];
-   int *ptr2 = ptr + 2;
-   int value2 = *ptr2;
-   int value3 = 3;
-   int *ptr3 = &value3;
+    char *begin = s;
+    char *end = s + len - 1;
+    while (begin < end) {
+        char tmp = *begin;
+        *begin = *end;
+        *end = tmp;
+        begin++;
+        end--;
+    }
+}
 
-   printf("value0 = %d\n", value0);
-   printf("value1 = %d\n", value1);
-   printf("value2 = %d\n", value2);
-   printf("value3 = %d\n", value3);
+int main(int argc, char *argv[])
+{
+    for (int i = 1; i < argc; i++) {
+        char *s = argv[i];
+        reverse(s, strlen(s));
+        printf("%s\n", s);
+    }
 
-   printf("array = %lld\n", (long long)array);
-   printf("ptr0 = %lld\n", (long long)ptr0);
-   printf("ptr2 = %lld\n", (long long)ptr2);
-   printf("ptr3 = %lld\n", (long long)ptr3);
-
-   return 0;
+    return 0;
 }
