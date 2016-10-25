7969cdeda279b6deeb34c6a6c43bfb11e2344b0c
Ivan Komissarov <ABBAPOH@gmail.com>
2016-10-25 22:13:06 +0300

    Fix list example

--- 4/0.list/main.c
+++ 4/0.list/main.c
@@ -18,6 +18,14 @@ struct Node *createNode(const char *name, struct Node *parent)
     return result;
 }
 
+void printList(struct Node *node)
+{
+    while (node) {
+        printf("name = %s\n", node->name);
+        node = node->next;
+    }
+}
+
 int main()
 {
     struct Node *first = createNode("first", 0);
@@ -25,11 +33,7 @@ int main()
     struct Node *third = createNode("third", second);
     struct Node *fourth = createNode("fourth", third);
 
-    struct Node *current = first;
-    while (current) {
-        printf("name = %s\n", current->name);
-        current = current->next;
-    }
+    printList(first);
 
     return 0;
 }
